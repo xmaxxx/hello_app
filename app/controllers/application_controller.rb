62af0310f801d0eea55b05ed899891b6963c873b
xmaxxx <xddomax@gmail.com>
2019-09-05 23:40:37 -0700

    update application.controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def hello
-		render html: "¡Hola, mundo"
+		render html: "This is my first rails application deployed to heroku! <p>This is a paragraph.</p>
+ "
 	end
 
 end
